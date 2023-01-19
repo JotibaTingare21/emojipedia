37b0d3632f18a5f7ea165af68c73bdbe46c69e51
JotibaTingare21 <jotibatingare21@gmail.com>
2023-01-19 15:47:28 +0530

    emojipedia

---
+++ src/emojipedia.js
@@ -0,0 +1,88 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🤓",
+    name: "Nerd Face",
+    meaning:
+      "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+  },
+  {
+    id: 5,
+    emoji: "😇",
+    name: "Smiling Face With Halo",
+    meaning:
+      "Would like to express its (possibly pretended) innocence. Someone does good deeds, is very sweet, innocent and behaves in an exemplary way. The angelic face can also be used humorously for rather not so good deeds or behaviors."
+  },
+  {
+    id: 6,
+    emoji: "👽",
+    name: "Alien",
+    meaning:
+      "The aliens are here! Symbol for an extraterrestrial being. However, smiles friendly and comes in peace."
+  },
+  {
+    id: 7,
+    emoji: "🙌",
+    name: "Person Raising Both Hands In Celebration",
+    meaning:
+      "Hallelujah! Hands are stretched upwards to celebrate. Is having a party, dancing wildly and friskily. Is in a good mood or having a lot of fun. Can also stand for “Banzai”, a Japanese cheer that brings luck and joy for 10,000 years."
+  },
+  {
+    id: 8,
+    emoji: "🗣",
+    name: "Talking Head",
+    meaning:
+      "The silhouette of a talking face. Can refer to rumors or to a person who likes to talk a lot. The news is already circulating!"
+  },
+  {
+    id: 9,
+    emoji: "👑",
+    name: "Crown",
+    meaning:
+      "“This is top” or “You are the best!” The crown embellished with precious jewels represents power and luxury. Is also known as king emoji."
+  },
+  {
+    id: 10,
+    emoji: "🙇",
+    name: "Person Bowing Deeply",
+    meaning:
+      "A person who is bowing to you. Is grateful because of something and deeply bows to you. Can also be an offering of reverence."
+  },
+  {
+    id: 11,
+    emoji: "🙈",
+    name: "See-No-Evil Monkey",
+    meaning:
+      "Monkey with two hands in front of its eyes. Closes its eyes to the world so as not to see anything bad or cruel, or is embarrassed. Mizaru is one of the three wise monkeys. In a sense, ignoring bad things."
+  },
+  {
+    id: 12,
+    emoji: "🧐",
+    name: "Face With Monocle",
+    meaning:
+      "A monocle is a visual aid with only one glass. At the end of the 19th century it was a status symbol. Something seems suspicious! The smiley makes a warning look around. The “upper-class“ version of the pondering smiley."
+  }
+];
+
+export default emojipedia;
